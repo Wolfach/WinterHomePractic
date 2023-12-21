ac876d0eea52b68ff777d9296dfe02dda9482d60
Wolfach <mega.pidikseev@mail.ru>
2023-12-21 21:08:45 +0400

    add edit, create and delete

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreList;
+use App\Models\ToDo;
+
+class ListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $affairs = ToDo::orderBy('id')->get();
+        return view('affairs', compact('affairs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreList $request)
+    {
+        ToDo::create($request->validated());
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreList $request, string $id)
+    {
+
+        $affairs = ToDo::findOrFail($id)->update($request->validated());
+        return redirect()->back();
+
+        // $affairs = ToDo::where($id)->update(['record'=>1]);
+        // return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $affairs = ToDo::findOrFail($id)->delete();
+        return redirect()->back();
+    }
+}
---
+++ resources/views/affairs.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <ul class="list-group">
+                        @foreach ( $affairs as $affair)
+                        <li class="list-group-item" style="display: flex; align-items: center;">
+                            <input type="checkbox">
+                                <div class="text" style="width: 900px;">{{$affair->record}}</div> <br>
+                                <form style="display: flex; align-items: center;" action="{{route('affairs.destroy',[$affair->id])}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class = 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150' type="submit" style="height: 20px">Удалить
+                                </button></form>
+                                <form style="display: flex; align-items: center;" action="{{route('affairs.update',[$affair->id])}}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <button class = 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150' type="submit" style="height: 20px; margin-left: 10px">Изменить
+                                </button>
+                                <input type="text" name="record" value="" class= 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                                </form>
+                        </li>
+                        @endforeach
+                      </ul>
+                      <form style="display: flex; align-items: center;" action="{{route('affairs.store',$affair)}}" method="POST">
+                        @csrf
+                      <button class = 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150' type="submit" style="margin-top: 10px">Добавить
+                    </button>
+                    <input type="text" name="record" value="" class= 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm'>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
